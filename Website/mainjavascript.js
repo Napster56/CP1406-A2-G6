82beab49bed69bba74bad9125467beee69333cf3
Harsh Dhadwal <harsh.dhadwal@my.jcu.edu.au>
2017-05-25 13:50:07 +1000

    javascript file

---
+++ Website/mainjavascript.js
@@ -0,0 +1,47 @@
+// Open navigation bar and posiotion top center
+function openNav() {
+  document.getElementById("navBox").style.width = "100%";					// expand nav bar
+  document.getElementById("navBox").style.textalign = "center";			// aling content center
+  document.getElementById("navImg").style.marginLeft = "50%";				// center violin image
+  document.getElementById("navBtn-1").style.marginLeft = "-480px";		// position button:  about us
+  document.getElementById("navBtn-2").style.marginLeft = "550px";			// position button:  concerts
+  document.getElementById("navBtn-3").style.marginLeft = "-600px";		// position button:  support us
+  document.getElementById("navBtn-4").style.marginLeft = "550px";			// position button:  artists
+  document.getElementById("navBtn-5").style.marginLeft = "-500px";		// position button:  contact us
+}
+
+// Close navigation bar. Move evrything to default position
+function closeNav() {
+  document.getElementById("navBox").style.width = "240px";
+  document.getElementById("navImg").style.marginLeft = "0px";
+  document.getElementById("navBtn-1").style.marginLeft = "0px";
+  document.getElementById("navBtn-2").style.marginLeft = "0px";
+  document.getElementById("navBtn-3").style.marginLeft = "0px";
+  document.getElementById("navBtn-4").style.marginLeft = "0px";
+  document.getElementById("navBtn-5").style.marginLeft = "0px";
+}
+
+// Delay opening link to allow nav bar transition
+function delay (URL) {
+    setTimeout( function() { window.location = URL }, 220 );
+}
+
+// modal for artists
+function closeall(){
+  document.getElementById("artistmodal1").style.display = "none";
+  document.getElementById("artistmodal2").style.display = "none";
+}
+
+function artistmodal1() {
+  document.getElementById("artistmodal1").style.display = "block";
+}
+
+function artistmodal2() {
+  document.getElementById("artistmodal2").style.display = "block";
+}
+
+span.onclick = closeall();
+
+window.onclick = closeall();
+
+window.onload = closeall();
